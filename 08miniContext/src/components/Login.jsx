3abff37b2f26477fa4a783a5d15bff8269afe678
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-07-05 00:54:02 +0530

    Context API and themeswitcher

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
 
     const {setUser} = useContext(UserContext)
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         setUser({username, password})
     }
